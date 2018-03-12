51f971c83d88b274782d842aebed4446a41450a4
Doug Cole <idugcoal@gmail.com>
2018-03-12 13:49:35 -0700

    Animates swipes and advances to next card

--- src/Deck.js
+++ src/Deck.js
@@ -2,7 +2,15 @@ import React, { Component } from 'react'
 import { View, Animated, PanResponder, Dimensions } from 'react-native'
 
 const SCREEN_WIDTH = Dimensions.get('window').width
+const SWIPE_THRESHOLD = 0.25 * SCREEN_WIDTH
+const SWIPE_OUT_DURATION = 250
+
 class Deck extends Component {
+  static defaultProps = {
+    onSwipeRight: () => {},
+    onSwipeLeft: () => {}
+  }
+  
   constructor(props) {
     super(props)
 
@@ -12,15 +20,44 @@ class Deck extends Component {
       onPanResponderMove: (event, gesture) => {
         position.setValue({ x: gesture.dx, y: gesture.dy })
       },
-      onPanResponderRelease: () => {}
-
+      onPanResponderRelease: (event, gesture) => {
+        if (gesture.dx > SWIPE_THRESHOLD) {
+          this.forceSwipe('right')
+        } else if (gesture.dx < -SWIPE_THRESHOLD) {
+          this.forceSwipe('left')
+        } else {
+          this.resetPosition()
+        }
+      }
     })
 
-    this.state = { panResponder, position }
+    this.state = { panResponder, position, index: 0 }
+  }
+
+  forceSwipe(direction) {
+    const x = direction === 'right' ? SCREEN_WIDTH : -SCREEN_WIDTH
+    Animated.timing(this.state.position, {
+      toValue: { x, y: 0},
+      duration: SWIPE_OUT_DURATION
+    }).start(() => this.onSwipeComplete(direction))
+  }
+
+  onSwipeComplete(direction) {
+    const { onSwipeLeft, onSwipeRight, data } = this.props
+    const item = data[this.state.index]
+
+    direction === 'right' ? onSwipeRight(item) : onSwipeLeft(item)
+    this.state.position.setValue({ x: 0, y: 0 })
+    this.setState({ index: this.state.index + 1 })
+  }
+
+  resetPosition() {
+    Animated.spring(this.state.position, {
+      toValue: { x: 0, y: 0}
+    }).start()
   }
 
   getCardStyle() {
-    console.log('hello')
     const { position } = this.state
     const rotate = position.x.interpolate({
       inputRange: [-SCREEN_WIDTH * 1.5, 0, SCREEN_WIDTH * 1.5],
@@ -34,8 +71,10 @@ class Deck extends Component {
   }
   
   renderCards() {
-    return this.props.data.map((item, index) => {
-      if (index === 0) {
+    return this.props.data.map((item, i) => {
+      if (i < this.state.index) { return null }
+      
+      if (i === this.state.index) {
         return (
           <Animated.View
             key={item.id} 
@@ -46,6 +85,7 @@ class Deck extends Component {
           </Animated.View>
         )
       }
+      
       return this.props.renderCard(item)
     })
   }
